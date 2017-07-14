2cf03dd1a97d537a5e7c15f76faca2003e6f2e5b
MrZWH <1875290692@qq.com>
2017-07-14 15:51:55 +0800

    first commit

---
+++ src/0.9585dade1b7630b6d953.hot-update.js
@@ -0,0 +1,62 @@
+webpackHotUpdate(0,{
+
+/***/ 184:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(49);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = __webpack_require__(57);
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var ComponentHeader = function (_React$Component) {
+    _inherits(ComponentHeader, _React$Component);
+
+    function ComponentHeader() {
+        _classCallCheck(this, ComponentHeader);
+
+        return _possibleConstructorReturn(this, (ComponentHeader.__proto__ || Object.getPrototypeOf(ComponentHeader)).apply(this, arguments));
+    }
+
+    _createClass(ComponentHeader, [{
+        key: 'render',
+        value: function render() {
+            return _react2.default.createElement(
+                'header',
+                null,
+                _react2.default.createElement(
+                    'h1',
+                    null,
+                    '\u8FD9\u91CC\u662F\u5934\u90E8\u554A'
+                )
+            );
+        }
+    }]);
+
+    return ComponentHeader;
+}(_react2.default.Component);
+
+exports.default = ComponentHeader;
+
+/***/ })
+
+})
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    context: path.join(__dirname),
+    devtool: debug ? "inline-sourcemap" : null,
+    entry: "./src/js/root.js",
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['react', 'es2015'],
+                        plugins: [["import", { "libraryName": "antd", "style": "css" }]]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+
+    output: {
+        path: __dirname,
+        filename: "./src/bundle.js"
+    },
+
+    plugins: debug ? [
+        new webpack.HotModuleReplacementPlugin(),
+    ] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false}),
+    ],
+
+    devServer: {
+        contentBase: './',
+        historyApiFallback: true,
+        inline: true,
+        hot: true,
+    },
+}
